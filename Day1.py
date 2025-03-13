f7db9b6c41c1022d026b5b47dc79268e0d2f2237
Eugenia <eugeniaasongo@gmail.com>
2025-03-13 13:51:29 +0000

    first commit

---
+++ Day1.py
@@ -0,0 +1,11 @@
+print("""Eugenia is my name,todays date is 13/03/2025.
+I am signing up for Relpit's 100 days of python
+challenge!
+I will make sure to spend time everyday coding 
+along,for a minimum of 1 hour a day.
+I'll be using Replit,an amazing online IDE so i can do
+this from my phone wherever I happen to be.No excuses
+from not coding from the middle of a field!
+I am feeling 🥳😚💃
+you can follow my progress at  my github
+""")
---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello")
+print("My name is Ama 💋❤")
+print("😂")
+
+print("""
+This was done by Eugenia
+        she is learning
+            This is good
+                👍
+""")
+print("what could go wrong?")
+
