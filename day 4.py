85ec11c3d73fc5e1d6f153fcb9d630b08a7d18bc
Eugenia <eugeniaasongo@gmail.com>
2025-03-16 21:14:05 +0000

    day4

---
+++ day 4.py
@@ -0,0 +1,14 @@
+print("===\033[33m Your Adventure Simulator\033[0m === ")
+print("""You'll be asked a bunch of questions 
+then we'll make you upan amazing 
+story with YOU as the star! 🤩⭐""")
+print()
+name = input("your name: ")
+enemy = input("Your worst enemy's name: ")
+superPower = input("Your super power: ")
+print()
+print("\033[36mOur story begins as our hero name approaches a foreboding castle......\033[0m")
+print("Suddenly a bolt of lightining striked the ground at the feet of", name)
+print ("\033[31m'Our final battle begins!'\033[0mshouts the evil", enemy,
+       "clearly missing the fact that", name , "has the power of\033[35m ",
+       superPower, "\033[0mwhich means they'll win quite easily")
