c5354b89c7677d22eb8e98260907271efda6577d
Eugenia <eugeniaasongo@gmail.com>
2025-03-16 21:41:05 +0000

    day5

---
+++ day5.py
@@ -0,0 +1,17 @@
+print("""\033[33mMARVEL MOVIE CHARACTER CREATOR
+--\033[0m""")
+question = input("Do you like 'hanging around'?: ")
+if question == "no":
+    print("Then you're not Spider-man😂😂")
+else:
+    print("Then you're Spider-man")
+question1 = input("Do you have a 'gravelly' voice?: ")
+if question1 == "no":
+    print("aww,then you're not korg")
+else:
+    print("aww,then you're korg")
+question2 = input("Do you often feel 'Marvelous😘'?: ")
+if question2 == "yes":
+    print("aha!You're Captain Marvel! Hi🙌!")
+else:
+    print("aha!You're not Captain Marvel😒!")
